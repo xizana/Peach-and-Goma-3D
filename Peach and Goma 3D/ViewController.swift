03e82ba877e85dcdb42e7ca821e18e4092adc2c8
xizana <xizoooo102@gmail.com>
2025-02-05 11:55:52 +0400

    Initial Commit

---
+++ Peach and Goma 3D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Peach and Goma 3D
+//
+//  Created by Lasha Khizanishvili on 05.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
